b4af17f5ce515b63636bedb6f2e28cb89b0a7d94
WHITE CODER <52422900+WhiteCooder@users.noreply.github.com>
2023-10-24 03:00:36 +0500

    Create imagesliderjs.js

---
+++ imagesliderjs.js
@@ -0,0 +1,97 @@
+ <section class="line-section">
+            <div class="r_seen_inner-2">
+              <div class="r_seen_swiper-2">
+                <div class="r_logo-container">
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-2 lozad" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/us_mag.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-2 lozad" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/marketwatch.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-3 lozad" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/g-news.svg" loading="lazy" alt="" width="180"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-4 lozad" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/ncn_news.svg" loading="lazy" alt="" width="130"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                </div>
+                <div class="r_logo-container">
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-2 lozad" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/us_mag.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-2 lozad" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/marketwatch.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-3 lozad" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/g-news.svg" loading="lazy" alt="" width="180"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-4 lozad" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/ncn_news.svg" loading="lazy" alt="" width="130"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com/cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                  <div class="r_content-holder">
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-8" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/logo_us_today.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-5" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/fox.svg" loading="lazy" alt="" width="47"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-6" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/cbs_news.svg" loading="lazy" alt="" width="100"></div>
+                    <div class="r_logo-slide-wrapper"><img class="logo-img-7" src="https://try.tranquil-air.com//cdn/landing-pages/191/assets/images/nbc.svg" loading="lazy" alt="" width="100"></div>
+                  </div>
+                </div>
+                <div class="w-embed" bind="1b714d23-6a97-fe60-0155-43bb960db8e4">
+                  <style>
+                    .r_logo-container {
+                        animation: track 60s linear forwards infinite;
+                        min-width: calc(560px * 5) !important
+                    }
+    
+                    @media screen and (max-width: 479px) {
+                        .r_logo-container {
+                            /*animation:track 50s linear infinite*/
+                        }
+                    }
+    
+                    @keyframes track {
+                        0% {
+                            transform: translateX(0)
+                        }
+                        100% {
+                            /*transform:translateX(calc(-560px * 5))*/
+                            transform: translateX(-100%)
+                        }
+                    }
+                  </style>
+                </div>
+              </div>
+              <div class="logo-center-el"><img class="star-icon" src="https://img.funnelish.com/10777/81633/1683010729-Untitled%20%281%29.png" loading="lazy" alt="">
+                <div class="line__text">12,500+ Reviews</div>
+              </div>
+            </div>
+          </section>
